018c5d31b2a15d3206f95ea89d6cf81a5c4b8525
robinbabu <robin636636@gmail.com>
2024-03-16 10:41:29 +0530

    Removed bluetooth part

--- lib/blue_print_pos.dart
+++ lib/blue_print_pos.dart
@@ -1,120 +1,21 @@
 import 'dart:developer';
 import 'dart:io';
-import 'dart:typed_data';
 import 'dart:ui';
 
-import 'package:blue_print_pos/models/models.dart';
 import 'package:blue_print_pos/receipt/receipt_section_text.dart';
-import 'package:blue_print_pos/scanner/blue_scanner.dart';
-import 'package:blue_thermal_printer/blue_thermal_printer.dart' as blue_thermal;
 import 'package:esc_pos_utils_plus/esc_pos_utils.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart' as flutter_blue;
-import 'package:flutter_blue_plus/gen/flutterblueplus.pb.dart' as proto;
 import 'package:image/image.dart' as img;
 import 'package:qr_flutter/qr_flutter.dart';
 
 class BluePrintPos {
-  BluePrintPos._() {
-    _bluetoothAndroid = blue_thermal.BlueThermalPrinter.instance;
-    _bluetoothIOS = flutter_blue.FlutterBluePlus.instance;
-  }
-
-  static BluePrintPos get instance => BluePrintPos._();
-
   static const MethodChannel _channel = MethodChannel('blue_print_pos');
-
-  /// This field is library to handle in Android Platform
-  blue_thermal.BlueThermalPrinter? _bluetoothAndroid;
-
-  /// This field is library to handle in iOS Platform
-  flutter_blue.FlutterBluePlus? _bluetoothIOS;
-
-  /// Bluetooth Device model for iOS
-  flutter_blue.BluetoothDevice? _bluetoothDeviceIOS;
-
-  /// State to get bluetooth is connected
-  bool _isConnected = false;
-
-  /// Getter value [_isConnected]
-  bool get isConnected => _isConnected;
-
-  /// Selected device after connecting
-  BlueDevice? selectedDevice;
-
-  /// return bluetooth device list, handler Android and iOS in [BlueScanner]
-  Future<List<BlueDevice>> scan() async {
-    return await BlueScanner.scan();
-  }
-
-  /// When connecting, reassign value [selectedDevice] from parameter [device]
-  /// and if connection time more than [timeout]
-  /// will return [ConnectionStatus.timeout]
-  /// When connection success, will return [ConnectionStatus.connected]
-  Future<ConnectionStatus> connect(
-    BlueDevice device, {
-    Duration timeout = const Duration(seconds: 5),
-  }) async {
-    selectedDevice = device;
-    try {
-      if (Platform.isAndroid) {
-        final blue_thermal.BluetoothDevice bluetoothDeviceAndroid =
-            blue_thermal.BluetoothDevice(
-                selectedDevice?.name ?? '', selectedDevice?.address ?? '');
-        await _bluetoothAndroid?.connect(bluetoothDeviceAndroid);
-      } else if (Platform.isIOS) {
-        _bluetoothDeviceIOS = flutter_blue.BluetoothDevice.fromProto(
-          proto.BluetoothDevice(
-            name: selectedDevice?.name ?? '',
-            remoteId: selectedDevice?.address ?? '',
-            type: proto.BluetoothDevice_Type.valueOf(selectedDevice?.type ?? 0),
-          ),
-        );
-        final List<flutter_blue.BluetoothDevice> connectedDevices =
-            await _bluetoothIOS?.connectedDevices ??
-                <flutter_blue.BluetoothDevice>[];
-        final int deviceConnectedIndex = connectedDevices
-            .indexWhere((flutter_blue.BluetoothDevice bluetoothDevice) {
-          return bluetoothDevice.id == _bluetoothDeviceIOS?.id;
-        });
-        if (deviceConnectedIndex < 0) {
-          await _bluetoothDeviceIOS?.connect();
-        }
-      }
-
-      _isConnected = true;
-      selectedDevice?.connected = true;
-      return Future<ConnectionStatus>.value(ConnectionStatus.connected);
-    } on Exception catch (error) {
-      print('$runtimeType - Error $error');
-      _isConnected = false;
-      selectedDevice?.connected = false;
-      return Future<ConnectionStatus>.value(ConnectionStatus.timeout);
-    }
-  }
-
-  /// To stop communication between bluetooth device and application
-  Future<ConnectionStatus> disconnect({
-    Duration timeout = const Duration(seconds: 5),
-  }) async {
-    if (Platform.isAndroid) {
-      if (await _bluetoothAndroid?.isConnected ?? false) {
-        await _bluetoothAndroid?.disconnect();
-      }
-      _isConnected = false;
-    } else if (Platform.isIOS) {
-      await _bluetoothDeviceIOS?.disconnect();
-      _isConnected = false;
-    }
-
-    return ConnectionStatus.disconnect;
-  }
 
   /// This method only for print text
   /// value and styling inside model [ReceiptSectionText].
   /// [feedCount] to create more space after printing process done
   /// [useCut] to cut printing process
-  Future<void> printReceiptText(
+  Future<List<int>> printReceiptText(
     ReceiptSectionText receiptSectionText, {
     int feedCount = 0,
     bool useCut = false,
@@ -133,14 +34,14 @@ class BluePrintPos {
       useCut: useCut,
       useRaster: useRaster,
     );
-    _printProcess(byteBuffer);
+    return byteBuffer;
   }
 
   /// This method only for print image with parameter [bytes] in List<int>
   /// define [width] to custom width of image, default value is 120
   /// [feedCount] to create more space after printing process done
   /// [useCut] to cut printing process
-  Future<void> printReceiptImage(
+  Future<List<int>> printReceiptImage(
     List<int> bytes, {
     int width = 120,
     int feedCount = 0,
@@ -156,7 +57,7 @@ class BluePrintPos {
       useRaster: useRaster,
       paperSize: paperSize,
     );
-    _printProcess(byteBuffer);
+    return byteBuffer;
   }
 
   /// This method only for print QR, only pass value on parameter [data]
@@ -176,40 +77,6 @@ class BluePrintPos {
       feedCount: feedCount,
       useCut: useCut,
     );
-  }
-
-  /// Reusable method for print text, image or QR based value [byteBuffer]
-  /// Handler Android or iOS will use method writeBytes from ByteBuffer
-  /// But in iOS more complex handler using service and characteristic
-  Future<void> _printProcess(List<int> byteBuffer) async {
-    try {
-      if (selectedDevice == null) {
-        print('$runtimeType - Device not selected');
-        return Future<void>.value(null);
-      }
-      if (!_isConnected && selectedDevice != null) {
-        await connect(selectedDevice!);
-      }
-      if (Platform.isAndroid) {
-        _bluetoothAndroid?.writeBytes(Uint8List.fromList(byteBuffer));
-      } else if (Platform.isIOS) {
-        final List<flutter_blue.BluetoothService> bluetoothServices =
-            await _bluetoothDeviceIOS?.discoverServices() ??
-                <flutter_blue.BluetoothService>[];
-        final flutter_blue.BluetoothService bluetoothService =
-            bluetoothServices.firstWhere(
-          (flutter_blue.BluetoothService service) => service.isPrimary,
-        );
-        final flutter_blue.BluetoothCharacteristic characteristic =
-            bluetoothService.characteristics.firstWhere(
-          (flutter_blue.BluetoothCharacteristic bluetoothCharacteristic) =>
-              bluetoothCharacteristic.properties.write,
-        );
-        await characteristic.write(byteBuffer, withoutResponse: true);
-      }
-    } on Exception catch (error) {
-      print('$runtimeType - Error $error');
-    }
   }
 
   /// This method to convert byte from [data] into as image canvas.
